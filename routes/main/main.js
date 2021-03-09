5e0c479e8ffc651ca7f6f3a992d84d4dcf10b74b
indjbackend <indj.najongjine@gmail.com>
2021-03-09 18:54:15 +0900

    웹 레이아웃 구성

--- routes/main/main.js
+++ routes/main/main.js
@@ -2,7 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", async (req, res) => {
-    return res.render("main.html");
+    try{
+        let title = []
+
+        for(let i=0; i<10; i++){
+            title.push("dummy");
+        }
+
+        return res.render("main.html", {title: title});
+    } catch(err){
+        console.log(err);
+    }
+    
 })
 
 module.exports = router
